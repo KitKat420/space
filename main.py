c2a3d57b8c1ad6037353be4e6c6a89f5851ac5f7
dante clark <danteclark11@hotmail.com>
2022-04-11 20:19:32 -0400

    refactored and added alien to window

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from turtle import back
 import pygame
 import sys
+from game_asset import GameAsset
 
 WIDTH, HEIGHT = 800, 600
 ORIGIN = (0, 0)
 BLACK = (0, 0, 0)
+FPS = 60
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
+pygame.init()
+clock = pygame.time.Clock()
 
 background = pygame.image.load("./assets/background.jpg", "background-img")
 scaled_bg = pygame.transform.scale(background, (800, 800))
-ship = pygame.image.load("./assets/ship.png", "ship-img")
-scaled_ship = pygame.transform.scale(ship, (100, 100))
 
 
-def display():
+ship = GameAsset('./assets/ship.png', 'ship_img')
+alien = GameAsset('./assets/alien.png', 'alien_img')
+
+
+def rects():
     WINDOW.fill(BLACK)
     pygame.Surface.blit(WINDOW, scaled_bg, ORIGIN)
-    pygame.Surface.blit(WINDOW, scaled_ship, ((
-        WIDTH - scaled_ship.get_width()) / 2, 500))
+    pygame.Surface.blit(WINDOW, ship.load_asset(), ((
+        WIDTH - ship.load_asset().get_width()) / 2, 500))
+    pygame.Surface.blit(WINDOW, alien.load_asset(), ((
+        WIDTH - ship.load_asset().get_width()) / 2, 20))
 
 
 def main():
@@ -27,8 +34,9 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        display()
+        rects()
         pygame.display.flip()
+        clock.tick(FPS)
 
 
 main()
