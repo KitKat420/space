84194baf8fb0dd3fa2f8710578353e946d7469ad
dante clark <danteclark11@hotmail.com>
2022-04-12 21:45:57 -0400

    alien can move

--- alien.py
+++ alien.py
@@ -1,4 +1,8 @@
+from re import L
 import pygame
+
+# WINDOW DIMENSIONS
+WIDTH, HEIGHT = 800, 600
 
 
 class Alien:
@@ -7,6 +11,16 @@ class Alien:
         self.hint = image_hint
         self.asset = pygame.image.load(image_path, image_hint)
         self.asset_rect = self.asset.get_rect()
+        self.direction = 5
 
     def load_asset(self):
         return pygame.transform.scale(self.asset, (100, 100))
+
+    def movement(self):
+        self.asset_rect.x += self.direction
+        if self.asset_rect.x == WIDTH - 100:
+            self.asset_rect.y += 10
+            self.direction *= -1
+        elif self.asset_rect.x == WIDTH - 800:
+            self.asset_rect.y += 10
+            self.direction *= -1
