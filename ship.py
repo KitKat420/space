88d447212751ee50ec6bce8827e380dd4a7cf77f
dante clark <danteclark11@hotmail.com>
2022-04-17 19:44:21 -0400

    refactored and added explosion animation

--- ship.py
+++ ship.py
@@ -3,15 +3,17 @@ from alien import Alien
 
 
 class Ship(Alien):
-    def __init__(self, image_path, image_hint):
-        self.image = image_path
-        self.hint = image_hint
-        super().__init__(self.image, self.hint)
-        load_bullet = pygame.image.load(
-            './assets/bullet.png', 'bullet_img')
-        self.bullet_asset = pygame.transform.scale(load_bullet, (100, 100))
-        self.bullet_rect = self.bullet_asset.get_rect()
-        self.did_fire = False
+    def __init__(self, char_asset, bullet_asset):
+        super().__init__(char_asset, bullet_asset)
+
+        # self.image = image_path
+        # self.hint = image_hint
+        # super().__init__(self.image, self.hint)
+        # load_bullet = pygame.image.load(
+        #     './assets/bullet.png', 'bullet_img')
+        # self.bullet_asset = pygame.transform.scale(load_bullet, (100, 100))
+        # self.bullet_rect = self.bullet_asset.get_rect()
+        # self.ship_rect =
 
     def controls(self, key_pressed):
         """this function takes input from keyboard and performs actions based on the key pressed."""
